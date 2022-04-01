d5e541278fec70e2b7485fc723b4daa2dae9dcd6
Dikkas <79013300+Dikkas@users.noreply.github.com>
2022-04-01 14:47:04 +0100

    Add files via upload

--- Books/js/main.js
+++ Books/js/main.js
@@ -1,22 +1,21 @@
 const released = document.getElementById("released");
 const nextBooks = document.getElementById("soon");
+const boughtBooks = document.getElementById("bought");
 const books = [];
 
 function getData() {
     fetch("./resources/db.json")
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             responseData(data.data);
         });
 }
 
 function responseData(data) {
     for (let i = 0; i < data.length; i++) {
-        let { title, day, month, year, editora, cover } = data[i];
-        books.push(objectCard(title, day, month, year, editora, cover));
+        let { title, day, month, year, editora, cover, bought } = data[i];
+        books.push(objectCard(title, day, month, year, editora, cover, bought));
     }
-    console.log(books);
     books.sort((a, b) => a.daysLeft - b.daysLeft);
     writeData(books);
 }
@@ -27,6 +26,15 @@ function writeData(data) {
             data[i];
         if (badge === "success") {
             released.innerHTML += createCard(
+                coverImg,
+                badge,
+                title,
+                relDate,
+                daysLeft,
+                editora
+            );
+        } else if (badge === "info") {
+            boughtBooks.innerHTML += createCard(
                 coverImg,
                 badge,
                 title,
@@ -52,7 +60,13 @@ function createCard(coverImg, badge, title, relDate, daysLeft, editora) {
         <div class="img-div"><img src="${coverImg}" class="card-img-top"></div>
         <div class="card-body">
             <div class="badge bg-${badge} mb-3">
-            ${daysLeft > 0 ? daysLeft + " days" : "Released"}
+            ${
+                daysLeft > 0
+                    ? daysLeft + " days"
+                    : badge == "info"
+                    ? "Bought"
+                    : "Released"
+            }
             </div>
             <h5 class="card-title">${title}</h5>
             <p class="card-text">${relDate}</p>
@@ -61,10 +75,7 @@ function createCard(coverImg, badge, title, relDate, daysLeft, editora) {
     </div>`;
 }
 
-function objectCard(title, day, month, year, editora, cover) {
-    let currentDate = new Date();
-    let badge = "danger";
-
+function objectCard(title, day, month, year, editora, cover, bought) {
     let relDate = `${day ? day + "/" : ""}${month ? month + "/" : ""}${year}`;
 
     // To set two dates to two variables
@@ -72,15 +83,20 @@ function objectCard(title, day, month, year, editora, cover) {
 
     // 0-11 is Month in JavaScript
     var date2 = new Date(year, month - 1 || 11, day || 31);
-    console.log(date2, title);
     // To calculate the time difference of two dates
     var Difference_In_Time = date2.getTime() - date1.getTime();
 
     // To calculate the no. of days between two dates
     let daysLeft = Math.ceil(Difference_In_Time / (1000 * 3600 * 24));
 
+    let badge = "danger";
+
     if (daysLeft <= 0) {
         badge = "success";
+    }
+
+    if (bought) {
+        badge = "info";
     }
 
     let coverImg = cover ? cover : "./resources/nocover.png";
